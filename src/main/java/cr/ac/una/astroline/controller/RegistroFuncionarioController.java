97b778e894cf36fc92a4cfa71614bb38f6b218b4
JekaCordero <jekacorderomontes@gmail.com>
2026-03-31 11:27:21 -0600

    FEAT: Avance 1 modo funcionario

---
+++ src/main/java/cr/ac/una/astroline/controller/RegistroFuncionarioController.java
@@ -0,0 +1,30 @@
+package cr.ac.una.astroline.controller;
+
+import cr.ac.una.astroline.util.FlowController;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author USUARIO UNA PZ
+ */
+public class RegistroFuncionarioController extends Controller implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @Override
+    public void initialize() {
+        // TO DO
+    }
+    
+}
